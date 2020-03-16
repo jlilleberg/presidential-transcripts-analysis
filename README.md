3b4ea51ed3059c0354d458307b2429975400d4d4
Joseph Lilleberg <jjglilleberg@hotmail.com>
2020-03-16 13:16:09 -0500

    update project files

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # presidential-transcripts-nlp-analysis
+
+Run the following python script to install the required packages
+```
+python install_packages.py
+```
---
+++ install_packages.py
@@ -0,0 +1,40 @@
+import os
+import subprocess
+import sys
+
+def pip_install(package):
+    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
+
+def path_install(package):
+	subprocess.check_call([sys.executable, "-m", "python", "spacy", package])
+
+pip_packages = ["adjustText", 
+				"beautifulsoup4", 
+				"bs4", 
+				"en-core-web-sm", 
+				"future", 
+				"gensim", 
+				"joblib", 
+				"nltk", 
+				"num2words", 
+				"numpy", 
+				"pandas",
+				"plotly", 
+				"pyLDAvis", 
+				"requests", 
+				"scattertext",
+				"scikit-learn", 
+				"scipy", 
+				"seaborn",
+				"spacy", 
+				"statsmodels", 
+				"text2num",
+				"tqdm"] 
+
+path_packages = ["python -m spacy download en_core_web_sm"]  
+
+for package in pip_packages:
+	pip_install(package)
+
+for package in path_packages:
+	os.system(package)
